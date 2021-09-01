21612fa3e1767a7cef6391f3669a8ea9ba316884
Jay Jain <jay.r.jain@gmail.com>
2021-08-31 20:23:44 -0400

    initial creation of project

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const axios = require('axios');
+const fetch = require('node-fetch');
+const Discord = require('discord.js');
+const { message } = require('discordjs-logger');
+const bot = new Discord.Client({intents:["GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILDS"]});
+const logger = require("discordjs-logger");
+const TOKEN = process.env.TOKEN;
+
+// logger.all(bot);
+
+bot.on('ready', () => {
+  console.log('Connecteed and ready.');
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('messageCreate', async msg => {
+    if (msg.content === 'kayne') {
+      const quote = await getQuote();
+      msg.reply(quote);
+    }
+  });
+
+const getQuote =  async() => {
+  try {
+    const response = await axios.get('https://api.kanye.rest/');
+    console.log(response.data.quote);
+    return JSON.parse(JSON.stringify(response.data.quote))
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+bot.login(TOKEN);
