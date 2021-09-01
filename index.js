01e9bcd148fbaf7b014835c56192255188fa471f
Jay Jain <jay.r.jain@gmail.com>
2021-08-31 21:38:12 -0400

    fixing the shiz

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const TOKEN = process.env.TOKEN;
 
 app.set('port', (process.env.PORT || 1337))
 
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 bot.on('ready', () => {
   console.log('Connecteed and ready.');
   console.info(`Logged in as ${bot.user.tag}!`);
